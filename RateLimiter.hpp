8b5bc5da2a5120ae48683ca9fed51877d4a0072f
Bernhard Specht <bernhard@specht.net>
2018-04-14 18:21:54 +0200

    google style

--- RateLimiter.hpp
+++ RateLimiter.hpp
@@ -8,57 +8,57 @@
 #include <chrono>
 
 class Bucket {
-private:
-    std::atomic<double> _zero_time;
-public:
-    Bucket(double zero_time = 0.0) :
-        _zero_time(zero_time) {};
-    bool aquire(double now, double rate, double burst_size, double consume = 1.0) {
-        auto zero_time_old = _zero_time.load();
-        double zero_time_new;
-        do {
-            double tokens = std::min((now - zero_time_old) * rate, burst_size);
-            if (tokens < consume) {
-                return false;
-            } else {
-                tokens -= consume;
-            }
-            zero_time_new = now - tokens / rate;
-        } while(!_zero_time.compare_exchange_weak(zero_time_old, zero_time_new));
+ private:
+  std::atomic<double> zero_time_;
+ public:
+  Bucket(double zero_time = 0.0) :
+      zero_time_(zero_time) {};
+  bool aquire(double now, double rate, double burst_size, double consume = 1.0) {
+    auto zero_time_old = zero_time_.load();
+    double zero_time_new;
+    do {
+      double tokens = std::min((now - zero_time_old) * rate, burst_size);
+      if (tokens < consume) {
+        return false;
+      } else {
+        tokens -= consume;
+      }
+      zero_time_new = now - tokens / rate;
+    } while(!zero_time_.compare_exchange_weak(zero_time_old, zero_time_new));
 
-        return true;
-    }
+    return true;
+  }
 };
 
 template <class T=std::string>
 class RateLimiter {
-private:
-    double _rate;
-    double _burst_size;
+ private:
+  double rate_;
+  double burst_size_;
 
-    std::unordered_map<T, std::unique_ptr<Bucket> > _key_to_bucket;
-    boost::shared_mutex _key_to_bucket_mu;
+  std::unordered_map<T, std::unique_ptr<Bucket> > key_to_bucket_;
+  boost::shared_mutex key_to_bucket_mu_;
 
-    static double now() {
-        using dur = std::chrono::duration<double>;
-        auto const now = std::chrono::steady_clock::now().time_since_epoch();
-        return std::chrono::duration_cast<dur>(now).count();
+  static double now() {
+    using dur = std::chrono::duration<double>;
+    auto const now = std::chrono::steady_clock::now().time_since_epoch();
+    return std::chrono::duration_cast<dur>(now).count();
+  }
+ public:
+  RateLimiter(double rate, double burst_size) :
+      rate_(rate),
+      burst_size_(burst_size) {};
+  bool aquire(std::string &key) {
+    boost::upgrade_lock<boost::shared_mutex> lock(key_to_bucket_mu_);
+
+    auto bucket_it = key_to_bucket_.find(key);
+    if (bucket_it == key_to_bucket_.end()) {
+      boost::upgrade_to_unique_lock<boost::shared_mutex> unique_lock(lock);
+      std::unique_ptr<Bucket> bucket(new Bucket());
+      key_to_bucket_[key] = std::move(bucket);
+      return true;
+    } else {
+      return bucket_it->second->aquire(now(), rate_, burst_size_);
     }
-public:
-    RateLimiter(double rate, double burst_size) :
-        _rate(rate),
-        _burst_size(burst_size) {};
-    bool aquire(std::string &key) {
-        boost::upgrade_lock<boost::shared_mutex> lock(_key_to_bucket_mu);
-
-        auto bucket_it = _key_to_bucket.find(key);
-        if (bucket_it == _key_to_bucket.end()) {
-            boost::upgrade_to_unique_lock<boost::shared_mutex> unique_lock(lock);
-            std::unique_ptr<Bucket> bucket(new Bucket());
-            _key_to_bucket[key] = std::move(bucket);
-            return true;
-        } else {
-            return bucket_it->second->aquire(now(), _rate, _burst_size);
-        }
-    };
+  };
 };
